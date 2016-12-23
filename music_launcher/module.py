e111c90265ba13eb899e1c60c34a1e9fc17800e4
AmarOk <blin.sebastien@enconn.fr>
2016-12-23 16:52:41 -0500

    localization

--- music_launcher/module.py
+++ music_launcher/module.py
@@ -2,12 +2,13 @@ from rori import RORIModule, RORIData
 
 class Module(RORIModule):
     def process(self, data):
+        string_to_say = self.rori.get_localized_sentence('playMusic')
         clients_music = self.rori.getClientFor(user="AmarOk", datatype="music", origin=data.client)
         clients_answer = self.rori.getClientFor(user="AmarOk", datatype="text", origin=data.client)
         if clients_music:
             self.rori.send(clients_music[0], RORIData(content="random", datatype="music"))
         if clients_answer:
-            self.rori.send(clients_answer[0], RORIData(content="J'allume la musique", datatype="text"))
+            self.rori.send(clients_answer[0], RORIData(content=string_to_say, datatype="text"))
         self.stop_processing = True
 
 m = Module()
--- music_launcher/rori/RORI.py
+++ music_launcher/rori/RORI.py
@@ -1,6 +1,25 @@
+import json
+
 class RORI:
+    def __init__(self):
+        self.lang = "en"
+
+    def set_language(self, new_lang):
+        self.lang = new_lang
+
     def getClientFor(self, user, datatype, origin):
         print("Not implemented!\n")
 
     def send(self, destination, data):
         print("Not implemented!\n")
+
+    def get_localized_sentence(self, id):
+            try:
+                with open('sentences.json') as f:
+                    data = f.read()
+                    json_data = json.loads(data)
+                    result = json_data[id][self.lang]
+                    return result
+                return ""
+            except:
+                return ""
