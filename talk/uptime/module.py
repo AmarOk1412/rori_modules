666f8cb76cc436b1069d1b70f042106a1a3d3d1c
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-05-12 17:21:01 -0400

    modules: add uptime module

---
+++ talk/uptime/module.py
@@ -0,0 +1,18 @@
+
+from datetime import timedelta
+from rori import EmotionsManager, Module
+
+class Module(Module):
+    def process(self, interaction):
+        '''Say since when the server is up'''
+        with open('/proc/uptime', 'r') as f:
+            uptime_seconds = float(f.readline().split()[0])
+            uptime_string = str(timedelta(seconds = uptime_seconds))
+            uptime_string = uptime_string[:uptime_string.rfind(':')].replace(':','h')
+        string_to_say = self.rori.get_localized_sentence('time', self.sentences) + uptime_string
+        self.rori.send_for_best_client("text/plain", interaction.author_ring_id, string_to_say)
+        # Update emotions
+        cjoy = EmotionsManager().get_emotions(interaction.author_ring_id)[1]
+        cjoy = 20 if cjoy > 20 else cjoy
+        EmotionsManager().go_to_emotion(ring_id=interaction.author_ring_id, delta=1, joy=cjoy)
+        self.stop_processing = True
